71dc29a2564a12665cb5bb495286fbdbd7ad71e3
damouse <mickey.barboi@gmail.com>
2016-10-07 10:06:41 -0500

    Commented out the crashy lines

--- Source/ProckFPS/BaseGameMode.cpp
+++ Source/ProckFPS/BaseGameMode.cpp
@@ -5,21 +5,16 @@
 #include "BaseGameMode.h"
 
 ABaseGameMode::ABaseGameMode() {
-	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Game mode loaded"));
+	  static ConstructorHelpers::FClassFinder<AActor> doorBPClassFinder(TEXT("Blueprint'/Game/Box'"));
 
-	// static ConstructorHelpers::FClassFinder<AActor> doorBPClassFinder(TEXT("Blueprint'/Game/Box'"));
-
-	// if (doorBPClassFinder.Class != nullptr) {
-	// 	boxBPClass = doorBPClassFinder.Class;
-	// }
+	  if (doorBPClassFinder.Class != nullptr) {
+	  	boxBPClass = doorBPClassFinder.Class;
+	  }
 }
 
 void ABaseGameMode::InitGameState() {
-
-	// // Create the new actor at the given position, cast it to the appropriate class, and call a method on it for testing
-	// FVector NewLocation = FVector(0.f, 0.f, 200.f);
-	// ABoxTest *box = (ABoxTest *) GetWorld()->SpawnActor<AActor>(boxBPClass, NewLocation, FRotator::ZeroRotator);
-	// box->SomethingHappened();
-
-	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Spawned BP"));
+	  // Create the new actor at the given position, cast it to the appropriate class, and call a method on it for testing
+	  FVector NewLocation = FVector(0.f, 0.f, 200.f);
+	  ABoxTest *box = (ABoxTest *) GetWorld()->SpawnActor<AActor>(boxBPClass, NewLocation, FRotator::ZeroRotator);
+	  box->SomethingHappened();
 }
