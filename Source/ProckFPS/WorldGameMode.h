c7ae8053597fc7b25edb3f11eab0ebf0498e380a
damouse <mickey.barboi@gmail.com>
2016-10-28 14:28:06 -0500

    playing with physics constraints

---
+++ Source/ProckFPS/WorldGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "WorldGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCKFPS_API AWorldGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
