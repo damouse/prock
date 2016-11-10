b4c61e15a282fed33ced0646e1e047be3108c42f
damouse <mickey.barboi@gmail.com>
2016-11-10 13:14:29 -0600

    merging boxactor and the blueprint seems ok as long as both get a chance to compile

--- Source/ProckFPS/BoxActor.cpp
+++ Source/ProckFPS/BoxActor.cpp
@@ -14,74 +14,10 @@ https://answers.unrealengine.com/questions/140298/attaching-particle-to-socket-o
 */
 
 ABoxActor::ABoxActor() {
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> SplinePipeAsset(TEXT("StaticMesh'/Game/Geometry/Meshes/S_Pipe_Spline.S_Pipe_Spline'"));
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> cubeStaticMesh(TEXT("StaticMesh'/Game/Geometry/Meshes/Cube.Cube'"));
-	//static ConstructorHelpers::FObjectFinder<UParticleSystem> beamClassFinder(TEXT("ParticleSystem'/Game/Particles/P_Beam.P_Beam'"));
-	//static ConstructorHelpers::FObjectFinder<UParticleSystem> beamActorClassFinder(TEXT("ParticleSystem'/Game/Particles/P_Beam_Actor.P_Beam_Actor'"));
-	//static ConstructorHelpers::FObjectFinder<UFont> fontAssetFinder(TEXT("Font'/Game/Inconsolata.Inconsolata'"));
-	//static ConstructorHelpers::FObjectFinder<UMaterial> fontMaterialFinder(TEXT("Material'/Game/M_Label.M_Label'"));
-
-	//PrimaryActorTick.bCanEverTick = true;
-
-	// The invisible root component
-	//UBoxComponent* root = CreateDefaultSubobject<UBoxComponent>(TEXT("RootComponent"));
-	//root->SetMobility(EComponentMobility::Movable);
-	//root->SetSimulatePhysics(true);
-	//root->SetEnableGravity(false);
-	//root->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
-	//RootComponent = root;
-
-	//// Visible translucent body
-	//cube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
-	//cube->SetupAttachment(RootComponent);
-
-	//// Centered label
-	//mainLabel = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Label"));
-	//mainLabel->SetTextRenderColor(FColor::White);
-	//mainLabel->SetText(FText::FromString(TEXT("MainLabel")));
-	//mainLabel->SetupAttachment(RootComponent);
-	//mainLabel->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f)); // Rotate so text faces the positive y direction
-
-	//// Set the preloaded assets
-	//if (cubeStaticMesh.Succeeded()) {
-	//	cube->SetStaticMesh(cubeStaticMesh.Object);
-	//}
-
-	//if (beamClassFinder.Succeeded()) {
-	//	particleBeamComponent = beamClassFinder.Object;
-	//}
-
-	//if (beamActorClassFinder.Succeeded()) {
-	//	particleBeamActorComponent = beamActorClassFinder.Object;
-	//}
-
-	//if (SplinePipeAsset.Succeeded()) {
-	//	splineStaticMesh = SplinePipeAsset.Object;
-	//}
-
-	//if (fontAssetFinder.Succeeded()) {
-	//	mainLabel->SetFont(fontAssetFinder.Object);
-	//}
-
-	//if (fontMaterialFinder.Succeeded()) {
-	//	mainLabel->SetTextMaterial(fontMaterialFinder.Object);
-	//}
-
-	// Create the lines
-	//AddEdge(FVector(0, 0, 0), FVector(100, 0, 0));
-	//AddEdge(FVector(0, 0, 0), FVector(0, 100, 0));
-	//AddEdge(FVector(0, 100, 0), FVector(100, 100, 0));
-	//AddEdge(FVector(100, 0, 0), FVector(100, 100, 0));
-
-	//AddEdge(FVector(0, 0, 100), FVector(100, 0, 100));
-	//AddEdge(FVector(0, 0, 100), FVector(0, 100, 100));
-	//AddEdge(FVector(0, 100, 100), FVector(100, 100, 100));
-	//AddEdge(FVector(100, 0, 100), FVector(100, 100, 100));
-
-	//AddEdge(FVector(0, 0, 0), FVector(0, 0, 100));
-	//AddEdge(FVector(0, 100, 0), FVector(0, 100, 100));
-	//AddEdge(FVector(100, 100, 0), FVector(100, 100, 100));
-	//AddEdge(FVector(100, 0, 0), FVector(100, 0, 100));
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> beamActorClassFinder(TEXT("ParticleSystem'/Game/Particles/P_Beam_Actor.P_Beam_Actor'"));
+	if (beamActorClassFinder.Succeeded()) {
+		particleBeamActorComponent = beamActorClassFinder.Object;
+	}
 }
 
 /*
@@ -91,7 +27,7 @@ the translucent portion of the box to match them.
 It should be looking at nested nodes to do this resolution. Right now its just checking the size of its text
 */
 FVector ABoxActor::SizeFitContents() {
-	return FVector();
+	//return FVector();
 
 	// Get the target size of the label and calculate a new size 
 	FVector labelSize = mainLabel->GetTextLocalSize();
@@ -112,52 +48,13 @@ FVector ABoxActor::SizeFitContents() {
 }
 
 void ABoxActor::SetText(char *text) {
-	//UE_LOG(LogProck, Log, TEXT("Setting label to: %s"), UTF8_TO_TCHAR(text));
-	SetMainLabelText(FText::FromString(FString(UTF8_TO_TCHAR(text))));
-	
-}
-
-// DEPRECATED
-// Draw a line (which must have already been instantiated in the constructor) at the given positions
-// All the commented out areas here are to allow adding lines at runtime. Currently disabled
-void ABoxActor::AddEdge(FVector start, FVector end) {
-	// Make a unique name for this beam and spline
-	char buffer[10];
-	sprintf(buffer, "Spline%d\0", (int)beams.size());
-	FString name = FString(ANSI_TO_TCHAR(buffer));
-
-	// Create the spline
-	USplineMeshComponent* spline = CreateDefaultSubobject<USplineMeshComponent>(FName(*(name)));
-	spline->SetMobility(EComponentMobility::Movable);
-	spline->SetupAttachment(RootComponent);
-
-	spline->ForwardAxis = ESplineMeshAxis::Z;
-	spline->SetMobility(EComponentMobility::Movable);
-	spline->SetStartAndEnd(start, FVector(0, 0, 0), end, FVector(0, 0, 0));
-	spline->SetStartScale(FVector2D(0.1, 0.1));
-	spline->SetEndScale(FVector2D(0.1, 0.1));
-
-	// Comment this on to set a mesh instead of a beam 
-	//spline->SetStaticMesh(splineStaticMesh);
-
-	// Create and initialize the beam
-	sprintf(buffer, "Beam%d\0", (int)beams.size());
-	name = FString(ANSI_TO_TCHAR(buffer));
-
-	//UParticleSystemComponent *beam = NewObject<UParticleSystemComponent>(this, FName(*(name)));
-	UParticleSystemComponent* beam = CreateDefaultSubobject<UParticleSystemComponent>(FName(*(name)));
-	beam->SetTemplate(particleBeamComponent);
-	beam->RegisterComponent();
-
-	// Store the beam and the spline
-	splines.push_back(spline);
-	beams.push_back(beam);
+	if (mainLabel) {
+		mainLabel->SetText(FString(UTF8_TO_TCHAR(text)));
+	}
 }
 
 // Create a beam attached to each actor 
 void ABoxActor::ConnectToBox(ABoxActor* other) {
-	return;
-
 	char buffer[10];
 	sprintf(buffer, "Connection%d\0", (int)connections.size());
 	FString name = FString(ANSI_TO_TCHAR(buffer));
@@ -170,12 +67,3 @@ void ABoxActor::ConnectToBox(ABoxActor* other) {
 
 	connections.push_back(beam);
 }
-
-//// Spawn a nested box in the coordinate space of its parent (for nesting boxes)
-//ABoxTest *child = (ABoxTest *)GetWorld()->SpawnActor<AActor>(boxBPClass);
-//child->AttachToComponent(box->GetRootComponent(), FAttachmentTransformRules::SnapToTargetIncludingScale);
-//child->SetActorScale3D(FVector(0.5f, 0.5f, 0.5f));
-
-//void ABoxActor::CursorOver() {
-//	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Overlap started"));
-//}
--- Source/ProckFPS/BoxActor.h
+++ Source/ProckFPS/BoxActor.h
@@ -20,49 +20,30 @@ public:
 	// Connect another box using 0,0 as both source and target
 	void ConnectToBox(ABoxActor* other);
 
-	// Add a new line to this box
-	void AddEdge(FVector start, FVector end);
-
-	// Change the text displayed in this box. TODO: 
-	// UFUNCTION(BlueprintCallable, Category = "SetText")
-	 void SetText(char *text);
+	// Change the text displayed in this box
+	void SetText(char *text);
 
 	// Resize this box to fit its contents by calling SizeToContents on all children, or 
-	// caluculating the dimensions of internal text. Returns the resized bounds
+	// caluculating the dimensions of internal text
 	FVector SizeFitContents();
 	
 	// The lines of this box
-	std::vector<UParticleSystemComponent *> beams;
-	std::vector<USplineMeshComponent *> splines;
 	std::vector<UParticleSystemComponent *> connections;
 
 	// Stored in the constructor. Not needed if not building lines at runtime
-	UParticleSystem *particleBeamComponent;
-	UParticleSystem *particleBeamActorComponent; // Same beam with connection settings set to actors instead of points
-	UStaticMesh *splineStaticMesh;
+	UParticleSystem *particleBeamActorComponent;
 
+	//
+	// References to components in the blueprint
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Label")
 	UTextRenderComponent* mainLabel;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cuber")
 	UStaticMeshComponent *cube;
 
-	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "SetMainLabelText"))
-	void SetMainLabelText(const FText& text);
-
-	// Used to animate the box up and down for testing
-	float RunningTime;
-
-
-	// Calls from the blueprint to cpp
-	//UFUNCTION(BlueprintCallable, Category = "Callbacks")
-	//	void CursorOver();
-
-	// Calls from cpp to the blueprint
-	//UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Happened"))
-	//	void SomethingHappened();
-
-	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
-	//	bool SpawnOnce;
+	//
+	// Functions that call into blueprints
+	// UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "SetMainLabelText"))
+	// void SetMainLabelText(const FText& text);
 };
 
