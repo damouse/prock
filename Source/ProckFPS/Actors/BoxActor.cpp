a28ac9f994b025b21f0ca71999aae32286b1dc66
damouse <mickey.barboi@gmail.com>
2017-01-04 15:58:48 -0600

    removed the experimental grass is greener

--- Source/ProckFPS/Actors/BoxActor.cpp
+++ Source/ProckFPS/Actors/BoxActor.cpp
@@ -1,37 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ProckFPS.h"
+
+#include "Actors/ScopeActor.h"
 #include "Actors/BoxActor.h"
 #include "Utils/Config.h"
+
 #include <stdio.h>
 #include <math.h> 
 
-#include "Actors/ScopeActor.h"
-
-#include "Components/SplineMeshComponent.h"
-
-using namespace std;
-
-void ABoxActor::BeginPlay() {
-	// Get the pathline points from blueprint
-	FVector startPos, startTan, endPos, endTan; 
-	GetPathInOut(startPos, startTan, endPos, endTan);
-	
-	// Spline right vectors-- automagical for now
-	FVector startRight(0, 1, 0);
-	FVector endRight(0, 1, 0);
-
-	for (int i = 0; i < 5; i++) {
-		USplineMeshComponent *seg = CreateSplineMeshComponent();
-		
-		FVector start(startPos + ((i * 3) * startRight));
-		FVector end(endPos + ((i * 3) * endRight));
-
-		seg->SetStartAndEnd(start, startTan, end, endTan);
-	}
-
-	UE_LOG(LogProck, Log, TEXT("Done"));
-}
 
 void ABoxActor::SetText(char *text) {
 	if (mainLabel) {
--- Source/ProckFPS/Actors/BoxActor.h
+++ Source/ProckFPS/Actors/BoxActor.h
@@ -1,10 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "GameFramework/Actor.h"
-#include <vector>
 #include "LinkableInterface.h"
+
+#include <vector>
 
 #include "BoxActor.generated.h"
 
@@ -32,16 +30,6 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cuber")
 	AScopeActor *scope;
-	
-	//
-	// Experimental-- lets take a quick detour and see if cpp is any clearer than blueprints
-	// It maybe, but at this point it requires backtracking on a lot of existing work in blueprints
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Pathline", meta = (DisplayName = "GetPathInOut"))
-	void GetPathInOut(FVector &startPos, FVector &startTan, FVector &endPos, FVector &endTan);
 
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Pathline", meta = (DisplayName = "CreateSplineMeshComponent"))
-	USplineMeshComponent* CreateSplineMeshComponent();
-
-	virtual void BeginPlay();
 };
 
